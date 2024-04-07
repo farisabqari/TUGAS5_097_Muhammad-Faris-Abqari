fcad1eb782c2b0fff16d5a7d4a3bf92ab1b28967
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-04-08 06:43:23 +0800

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Ini Video Mba Kunti");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("Ini Audio Mba Kunti");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("Ini Gambar Mba Kunti");
+});
